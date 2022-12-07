612ceb2cfa6fc589634e06218b854fca97445756
TypicalDad <119784728+TypicalDad@users.noreply.github.com>
2022-12-07 09:49:31 +0300

    Update AliquotNumbers.java

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
+        System.out.println("Aliquot");
+        System.out.println("Not aliquot");
     }
 }
