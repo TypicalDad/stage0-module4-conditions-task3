807452b3cdb66757349b1dd1219f30efd964b3c7
TypicalDad <119784728+TypicalDad@users.noreply.github.com>
2022-12-07 09:42:28 +0300

    Update Seasons.java

--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,16 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
-        if(month>= 0 && month <3){
+        if(month>= 0 && month <3 || month == 12){
             System.out.println("Winter");
         }
         else if(month >= 3 && month <6){
             System.out.println("Spring");
         }
-        else if(month>= 6 && month <10){
+        else if(month>= 6 && month <9){
             System.out.println("Summer");
         }
-        else if(month>= 10 && month <13){
+        else if(month>= 9 && month <12){
             System.out.println("Autumn");
         }
         else{
