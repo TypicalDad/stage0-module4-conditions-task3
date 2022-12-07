5b7e656ab0a6a7c6c45254b93c7ad40548a8ed15
TypicalDad <119784728+TypicalDad@users.noreply.github.com>
2022-12-07 10:12:43 +0300

    Update IsEnglishSymbolDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.task3;
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
         int oneMore = (int) symbol;
-        if(oneMore>40 && oneMore <123){
+        if(oneMore>64 && oneMore <123){
             System.out.println("English");
         }
         else{
